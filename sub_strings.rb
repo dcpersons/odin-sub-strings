9764f436c396f40c335a7c170693b5256e03a4ee
dcpersons <danielcpersons@gmail.com>
2026-06-21 11:31:01 -0400

    Update solution to make more readable

--- sub_strings.rb
+++ sub_strings.rb
@@ -1,6 +1,10 @@
 def substrings(str, dictionary)
   dictionary.reduce(Hash.new) do |hash, word|
-    hash[word] = str.downcase.scan(word).length unless str.downcase.scan(word).length == 0
+    hits = str.downcase.scan(word).length
+    hash[word] = hits unless hits == 0
     hash
   end
 end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+p substrings("Howdy partner, sit down! How's it going?", dictionary)
